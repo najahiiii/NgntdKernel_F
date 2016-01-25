ee36c68c7050bb397e7262f4486dc3a11279722a
Shoaib0597 <Shoaib0595@gmail.com>
2016-01-25 16:23:49 +0530

    Optimized task_sched_runtime for upto 20% increase in Performance
    Signed-off-by: Ayra Hikari <AyraHikari@gmail.com>
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5983,10 +5983,23 @@ static bool early_detection_notify(struct rq *rq, u64 wallclock)
 		if (!loop_max)
 			break;
 
-		if (wallclock - p->last_wake_ts >= EARLY_DETECTION_DURATION) {
-			rq->ed_task = p;
-			return 1;
-		}
+#if defined(CONFIG_64BIT) && defined(CONFIG_SMP)
+ /*
+	* 64-bit doesn't need locks to atomically read a 64bit value.
+	* So we have a optimization chance when the task's delta_exec is 0.
+	* Reading ->on_cpu is racy, but this is ok.
+	*
+	* If we race with it leaving cpu, we'll take a lock. So we're correct.
+	* If we race with it entering cpu, unaccounted time is 0. This is
+	* indistinguishable from the read occurring a few cycles earlier.
+	*/
+ if (!p->on_cpu)
+ return p->se.sum_exec_runtime;
+#endif
+
+	rq = task_rq_lock(p, &flags);
+	ns = p->se.sum_exec_runtime + do_task_delta_exec(p, rq);
+	task_rq_unlock(rq, p, &flags);
 
 		loop_max--;
 	}

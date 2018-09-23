42818db8bbb1c032c8b6743756033813610d716b
najahiiii <tlogitechnjhi@gmail.com>
2018-09-24 04:52:18 +0700

    drivers: cpufreq: add zzmoove cpu governor and enable at defconfig
    Co-authored-by: Zane Zaminsky <cyxman@yahoo.com>
    Signed-off-by: najahiiii <tlogitechnjhi@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -521,10 +521,13 @@ extern struct cpufreq_governor cpufreq_gov_lisi;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_lisi)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 extern struct cpufreq_governor cpufreq_gov_cafactive;
-#define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_cafactive)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_cafactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_PEGASUSQ)
 extern struct cpufreq_governor cpufreq_gov_pegasusq;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_pegasusq)
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_pegasusq)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ZZMOOVE)
+extern struct cpufreq_governor cpufreq_gov_zzmoove;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_zzmoove)
 #endif
 
 /*********************************************************************
